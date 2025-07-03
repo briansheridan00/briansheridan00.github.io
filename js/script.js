b44bfc83eaa6ad4ff2bd15ed33e66c7a170efa66
Brian Sheridan <brian.sheridan272@gmail.com>
2025-07-03 21:18:14 +0200

    Navigation bar added and typos fixed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-// Scripts to be added here 
+// Scripts to be added here // 
+
+
+// Responsive navigation bar  
+const toggle = document.getElementById('nav-toggle');
+const links = document.getElementById('nav-links');
+
+toggle.addEventListener('click', () => {
+links.classList.toggle('active');
+}); 
